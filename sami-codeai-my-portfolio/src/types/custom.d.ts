84f92774efec0fe9a9886c3b087b61c5bcd732a8
sami-codeai <sami.codeai@gmail.com>
2025-12-18 17:26:24 +0530

    favicon added

--- sami-codeai-my-portfolio/src/types/custom.d.ts
+++ sami-codeai-my-portfolio/src/types/custom.d.ts
@@ -7,3 +7,18 @@ declare module '*.scss' {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
